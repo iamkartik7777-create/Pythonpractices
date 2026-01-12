e7d47d84d5a77fc2d316b5daf94a70dccb2a72d8
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-12 22:37:20 +0530

    • Create a dictionary of 3 students with name as key and marks as value • Print the highest and lowest marks • Convert all marks into a set and remove duplicates • Print the final set

--- day002.py
+++ day002.py
@@ -1,10 +1,29 @@
-nums = []
-for i in range(5):
-    nums.append(int(input("Enter number: ")))
+# nums = []
+# for i in range(5):
+#     nums.append(int(input("Enter number: ")))
 
-print("Largest:", max(nums))
-print("Smallest:", min(nums))
-print("Sum:", sum(nums))
+# print("Largest:", max(nums))
+# print("Smallest:", min(nums))
+# print("Sum:", sum(nums))
 
-unique_nums = list(set(nums))
-print("Without duplicates:", unique_nums)
+# unique_nums = list(set(nums))
+# print("Without duplicates:", unique_nums)
+
+
+
+
+
+
+students = {
+    "Rahul": 85,
+    "Aman": 90,
+    "Neha": 85
+}
+
+marks = students.values()
+
+print("Highest marks:", max(marks))
+print("Lowest marks:", min(marks))
+
+marks_set = set(marks)
+print("Final set of marks:", marks_set)
