83a91bb0f03c83530a417a36bb4f34ad7a6f3b25
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-14 21:24:29 +0530

    another code.

--- day003.py
+++ day003.py
@@ -1,8 +1,28 @@
-days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-num = int(input("Enter week number (1-7): "))
+# num = int(input("Enter week number (1-7): "))
 
-if 1 <= num <= 7:
-    print(days[num-1])
+# if 1 <= num <= 7:
+#     print(days[num-1])
+# else:
+#     print("Invalid input")
+
+
+day = int(input("Enter week number (1-7): "))
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
 else:
-    print("Invalid input")
+    print("Invalid input! Please enter a number between 1 and 7.")
