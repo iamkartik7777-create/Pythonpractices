94f9990f7657f611cf34489141a9243242493026
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-14 21:31:23 +0530

    Another Python code (Months)

--- day003.py
+++ day003.py
@@ -36,15 +36,52 @@
 
 
 #Python Code (Months)
-months = [
-    "January", "February", "March", "April",
-    "May", "June", "July", "August",
-    "September", "October", "November", "December"
-]
+# months = [
+#     "January", "February", "March", "April",
+#     "May", "June", "July", "August",
+#     "September", "October", "November", "December"
+# ]
 
-num = int(input("Enter month number (1-12): "))
+# num = int(input("Enter month number (1-12): "))
 
-if 1 <= num <= 12:
-    print(months[num-1])
+# if 1 <= num <= 12:
+#     print(months[num-1])
+# else:
+#     print("Invalid input")
+
+
+
+
+month = int(input("Enter month number (1-12): "))
+
+if month == 1:
+    print("January")
+elif month == 2:
+    print("February")
+elif month == 3:
+    print("March")
+elif month == 4:
+    print("April")
+elif month == 5:
+    print("May")
+elif month == 6:
+    print("June")
+elif month == 7:
+    print("July")
+elif month == 8:
+    print("August")
+elif month == 9:
+    print("September")
+elif month == 10:
+    print("October")
+elif month == 11:
+    print("November")
+elif month == 12:
+    print("December")
 else:
-    print("Invalid input")
+    print("Invalid input! Please enter a number between 1 and 12.")
+
+
+
+
+
