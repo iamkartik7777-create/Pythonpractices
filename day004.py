1f0c9a9dd08f013b05d1e0e824f68b8534817de8
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-15 23:09:14 +0530

    All function (very easy to understand)

---
+++ day004.py
@@ -0,0 +1,107 @@
+#1. Input / Output Functions
+print()
+input()
+
+#2. Type Conversion Functions
+int()
+float()
+str()
+bool()
+complex()
+list()
+tuple()
+set()
+dict()
+
+#3. Numeric Functions
+abs()
+pow()
+round()
+min()
+max()
+sum()
+divmod()
+
+
+
+
+#4. Sequence Functions (List, Tuple, String)
+len()
+sorted()
+reversed()
+enumerate()
+zip()
+slice()
+
+
+
+
+
+#5. Character & String Functions
+chr()
+ord()
+
+
+
+
+#6. Logical / Boolean Functions
+all()
+any()
+
+
+
+#7. Type Checking Functions
+type()
+isinstance()
+
+
+
+
+#8. VIteration Functions
+range()
+next()
+iter()
+
+
+#9. Object & Memory Functions
+id()
+hash()
+dir()
+help()
+
+
+#10. Mathematical Functions
+bin()
+oct()
+hex()
+
+
+
+#11. Execution & Compilation
+eval()
+exec()
+compile()
+
+
+
+
+#12. File Handling
+open()
+
+
+
+#13. Class / Attribute Functions
+getattr()
+setattr()
+delattr()
+hasattr()
+
+
+
+#14. Miscellaneous Functions
+callable()
+format()
+frozenset()
+globals()
+locals()
+vars()
