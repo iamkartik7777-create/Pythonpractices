f6eab401e11c4c00ee822510abae991f4e428177
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-16 22:53:19 +0530

    LOOP
    for ,while

---
+++ day005.py
@@ -0,0 +1,41 @@
+#1: Print numbers 1 to 5
+for i in range(1, 6):
+    print(i)
+
+
+#2: Print each character of a string
+name = "Python"
+for ch in name:
+    print(ch)
+
+
+#3: Print even numbers
+for i in range(2, 11, 2):
+    print(i)
+
+
+
+
+                   #WHILE
+#Print numbers 1 to 5
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+                   
+
+#2: Countdown
+n = 5
+while n > 0:
+    print(n)
+    n -= 1
+
+
+                 #Loop Control Statements
+#break (stop loop)
+for i in range(1, 10):
+    if i == 5:
+        break
+    print(i)
+
+                 
