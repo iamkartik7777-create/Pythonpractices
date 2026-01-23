fd20b0b3e0dfbd617147de382244a7049a9069f6
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-23 22:43:38 +0530

    pratices py

---
+++ day011.py
@@ -0,0 +1,13 @@
+name = "kartik singh"
+age = 22
+height =180.5
+
+# print(type(name))
+# print(type(age))
+# print(type(height))
+
+# print("My name is :",(name))
+# print("age is :",age)
+# print("and my height is :",(height))
+
+# print(len(name))
