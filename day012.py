e64f8c307a810874735d06afdcb9fd8b144b79f0
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-24 23:54:11 +0530

    Using Comparison Operators
    Comparison operators with if & else

---
+++ day012.py
@@ -0,0 +1,19 @@
+# a = 10
+# b = 5
+
+# print(a == b)   # False
+# print(a != b)   # True
+# print(a > b)    # True
+# print(a < b)    # False
+# print(a >= b)   # True
+# print(a <= b)   # False
+
+                      #Using Comparison Operators with if
+# age = 18
+
+# if age >= 18:
+#     print("You are eligible to vote")
+# else:
+#     print("Not eligible")
+
+
