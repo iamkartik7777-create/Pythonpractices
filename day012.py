02a6b06d9aa35d73ddda14586ceae3d62f316f93
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-24 23:58:07 +0530

    Real-Life Example
    Real-Life Example Like password & Eligible to vote

--- day012.py
+++ day012.py
@@ -1,19 +1,26 @@
-# a = 10
-# b = 5
+a = 10
+b = 5
 
-# print(a == b)   # False
-# print(a != b)   # True
-# print(a > b)    # True
-# print(a < b)    # False
-# print(a >= b)   # True
-# print(a <= b)   # False
+print(a == b)   # False
+print(a != b)   # True
+print(a > b)    # True
+print(a < b)    # False
+print(a >= b)   # True
+print(a <= b)   # False
 
                       #Using Comparison Operators with if
-# age = 18
+age = 18
 
-# if age >= 18:
-#     print("You are eligible to vote")
-# else:
-#     print("Not eligible")
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("Not eligible")
 
+                                   #Real-Life Example
+password = "python123"
+
+if password == "python123":
+    print("Login successful")
+else:
+    print("Wrong password")
 
