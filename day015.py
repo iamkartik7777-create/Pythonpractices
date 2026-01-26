64178e2d017c820c081fad2fc31bb05fca73a783
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-26 23:06:53 +0530

    Check positive, negative, or zero
    Check positive, negative, or zero with the
    use by if-elif-elsa

--- day015.py
+++ day015.py
@@ -4,3 +4,14 @@ if num % 2 == 0:
     print("Even number")
 else:
     print("Odd number")
+
+
+                   #Check positive, negative, or zero (if–elif–else)
+num = float(input("Check positive, negative, or zero :"))
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
