dde5f814d8bb9cb784a7e9e27b090e94d6204243
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-26 23:13:31 +0530

    Student pass or fail
    Grade system example
    and
    Student pass or fail

--- day015.py
+++ day015.py
@@ -1,4 +1,5 @@
-num = 7
+                 #Even or Odd number
+num =int(input("Even or Odd number:"))
 
 if num % 2 == 0:
     print("Even number")
@@ -15,3 +16,29 @@ elif num < 0:
     print("Negative number")
 else:
     print("Zero")
+
+
+                #Student pass or fail
+marks =float(input("Enter Your marks :"))
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+                 #Grade system example
+marks = 85
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Grade D")
+  
+
+
+
+          
