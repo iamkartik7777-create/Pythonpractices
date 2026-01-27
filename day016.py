15ae27e505044371c403d5f89eec97324d2bda4a
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-27 23:59:22 +0530

    LARGEST NO. / A,B,,C,D, GREAD (MARK)
    LARGEST NO. / A,B,,C,D, GREAD (MARKS) RESULTS

--- day016.py
+++ day016.py
@@ -10,3 +10,30 @@ if num == 1 :
 elif num == 1 :
     print( 1 )
 
+#Q1
+num=300
+num2=50
+num3=90
+
+if num>num2 and num>num3:
+    print("num is largest")
+elif num2>num and num2>num3:
+    print("num2 is largest")
+else:
+    print("num3 is largest")
+
+#Q2
+marks=int(input("Enter your marks"))
+if marks>=80 and marks<=100:
+    print("A Grade")
+elif marks>=60 and marks<80:
+    print("B Grade")
+elif marks>=30 and marks<60:
+    print("C Grade")
+elif marks>=0 and marks<30:
+    print("D Grade")
+else:
+    print("Invlid Input")
+
+
+
