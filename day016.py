d8aff6c568932760c8e031f2dca808afe71e0481
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-27 23:55:52 +0530

    PASS/FAIL ( IF ELIF )

---
+++ day016.py
@@ -0,0 +1,12 @@
+# num=float(input("Enter your marks :"))
+# if num >= 40:
+#     print("YOU ARE PASSED ",(num))
+# else:
+#     print("YOU ARE FAIL",(num))
+
+num = int(input("ENTER YOUR NO. :"))
+if num == 1 :
+    print(1)
+elif num == 1 :
+    print( 1 )
+
