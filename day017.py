010fc3d7a08bfaf8fb2ebea75efb4cfd1dcc4154
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-30 23:54:31 +0530

    triangle
    Increasing Number Triangle

--- day017.py
+++ day017.py
@@ -5,4 +5,8 @@ for i in range(1, rows + 1):
         print("*", end=" ")
     print()
 
-    
+# Increasing Number Triangle    
+for i in range(1, 5):
+    for j in range(1, i+1):
+        print(j, end="")
+    print()
