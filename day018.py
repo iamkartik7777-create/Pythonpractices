3c4ee185ce02f4f60ce7d319f53a8fcff83c53b6
iamkartik7777-create <iamkartik7777@gmail.com>
2026-01-31 23:59:49 +0530

    new

--- day018.py
+++ day018.py
@@ -4,3 +4,15 @@ for i in range(rows):
     print(" " * (rows - i - 1), end="")
     # Print stars
     print("*" * (2 * i + 1))
+
+
+size = 5
+
+for i in range(size):
+    for j in range(size):
+        # Print stars only if it's the first or last row/column
+        if i == 0 or i == size - 1 or j == 0 or j == size - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print() 
