6f4279276365b22612764d15f190aad1b775dd6a
iamkartik7777-create <iamkartik7777@gmail.com>
2026-02-01 23:28:48 +0530

    asd

--- day018.py
+++ day018.py
@@ -1,18 +1,34 @@
+# rows = 5
+# for i in range(rows):
+#     # Print spaces
+#     print(" " * (rows - i - 1), end="")
+#     # Print stars
+#     print("*" * (2 * i + 1))
+
+
+# size = 5
+
+# for i in range(size):
+#     for j in range(size):
+#         # Print stars only if it's the first or last row/column
+#         if i == 0 or i == size - 1 or j == 0 or j == size - 1:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print() 
+
+
 rows = 5
 for i in range(rows):
-    # Print spaces
-    print(" " * (rows - i - 1), end="")
-    # Print stars
-    print("*" * (2 * i + 1))
-
-
-size = 5
-
-for i in range(size):
-    for j in range(size):
-        # Print stars only if it's the first or last row/column
-        if i == 0 or i == size - 1 or j == 0 or j == size - 1:
-            print("*", end=" ")
+    # Print leading spaces
+    for j in range(rows - i - 1):
+        print(" ", end="")
+    
+    # Print stars/hollow spaces
+    for k in range(2 * i + 1):
+        # Condition: first star, last star, or the entire last row
+        if k == 0 or k == (2 * i) or i == rows - 1:
+            print("*", end="")
         else:
-            print(" ", end=" ")
-    print() 
+            print(" ", end="")
+    print()
