5c214b621f2f6ab34a777438550bc8ca7ed88c36
iamkartik7777-create <iamkartik7777@gmail.com>
2026-02-04 00:24:46 +0530

    Different Marks in a if-elif-elsa

--- day019.py
+++ day019.py
@@ -5,10 +5,25 @@
 
 # Condition: Agar temperature 30 se zyada hai, toh print karo "It is hot today."
 # Else (Warna): Print karo "It is a nice day."
+# tem = int(input("Temperature is :"))
+# if tem>30:
+#          print ("It is hot today.")
 
-tem = int(input("Temperature is :"))
-if tem>30:
-    print ("It is hot today.")
+# else:
+#     print("It is a nice day.")
 
+
+# Agar Marks 90 se zyada hain: Print karo "Excellent!"
+# Agar Marks 40 se 90 ke beech hain: Print karo "You passed."
+# Else (40 se kam): Print karo "Better luck next time."
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Excellent!")
+elif marks >= 40:
+    print("You passed.")
 else:
-   print("It is a nice day.")
+    print("Better luck next time.")
+            
+
