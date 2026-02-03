c7766642a23d2ae37f9ec21686c28fe3a588c07e
iamkartik7777-create <iamkartik7777@gmail.com>
2026-02-03 23:56:17 +0530

    Temperature with the help of if-elsa

--- day019.py
+++ day019.py
@@ -1,4 +1,14 @@
-name = input("ENTER YOUR NAME IS :")
-age = input("ENTER YOUR AGE IS :")
+# name = input("ENTER YOUR NAME IS :")
+# age = input("ENTER YOUR AGE IS :")
 
-print(f"Hello {name} you are {age} years old. ")
+# print(f"Hello {name} you are {age} years old. ")
+
+# Condition: Agar temperature 30 se zyada hai, toh print karo "It is hot today."
+# Else (Warna): Print karo "It is a nice day."
+
+tem = int(input("Temperature is :"))
+if tem>30:
+    print ("It is hot today.")
+
+else:
+   print("It is a nice day.")
