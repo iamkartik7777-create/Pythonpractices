073c07524c9fed8ff7c7dd310c00943c0c4bd7ba
iamkartik7777-create <iamkartik7777@gmail.com>
2026-02-07 23:51:50 +0530

    LOGIN SYSTEM

--- day020.py
+++ day020.py
@@ -6,10 +6,11 @@
 # else:
 #     print("  garm hai Temperture  ")
 
+#LOGIN SYSTEM(ID WITH PASSWORD)
+# id_account = str(input(" Enter your ID:  "))
+# id_pass = str(input(" Enter your Password:  "))
+# if id_account=="kartik" and  id_pass=="pin2004":
+#     print("Login Successful")
+# else:
+#     print("Try Again ")
 
-id_account = str(input(" Enter your ID:  "))
-id_pass = str(input(" Enter your Password:  "))
-if id_account=="kartik" and  id_pass=="pin2004":
-    print("welcome page")
-else:
-    print("Try again ")
