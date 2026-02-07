4ed4946417104987ec6229f69e62850a8661d246
iamkartik7777-create <iamkartik7777@gmail.com>
2026-02-07 23:56:20 +0530

    Age Group Classifier

--- day021.py
+++ day021.py
@@ -6,6 +6,22 @@ for i in range(1, num + 1):
 
 print("Output:", factorial)
 
+
+
 num = int(input("Enter a number: "))
 count = len(str(abs(num))) 
 print("Output:", count)
+
+
+age = int(input("Enter your age: "))
+
+if 0 <= age <= 12:
+    print("Child")
+elif 13 <= age <= 19:
+    print("Teenager")
+elif 20 <= age <= 59:
+    print("Adult")
+elif age >= 60:
+    print("Senior Citizen")
+else:
+    print("Invalid age")
