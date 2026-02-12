cd2d8f50c89d3d37dd02e8027798132a15e42c42
iamkartik7777-create <iamkartik7777@gmail.com>
2026-02-12 23:24:59 +0530

    "Turtle Graphics"

---
+++ day023.py
@@ -0,0 +1,23 @@
+import turtle
+import colorsys
+
+
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor("black")
+t.speed(0) 
+
+n = 36 
+h = 0  
+
+for i in range(400):
+    
+    c = colorsys.hsv_to_rgb(h, 1, 0.8)
+    h += 1/n
+    t.color(c)
+    
+    t.forward(i * 2)
+    t.left(145) 
+    t.width(i/100)
+
+turtle.done()
