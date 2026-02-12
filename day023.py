efe7242cb8517a46f4721cb00e57d19abd3af988
iamkartik7777-create <iamkartik7777@gmail.com>
2026-02-12 23:55:55 +0530

    def

--- day023.py
+++ day023.py
@@ -1,23 +1,29 @@
-import turtle
-import colorsys
+# import turtle
+# import colorsys
 
 
-t = turtle.Turtle()
-s = turtle.Screen()
-s.bgcolor("black")
-t.speed(0) 
+# t = turtle.Turtle()
+# s = turtle.Screen()
+# s.bgcolor("black")
+# t.speed(0) 
 
-n = 36 
-h = 0  
+# n = 36 
+# h = 0  
 
-for i in range(400):
+# for i in range(400):
     
-    c = colorsys.hsv_to_rgb(h, 1, 0.8)
-    h += 1/n
-    t.color(c)
+#     c = colorsys.hsv_to_rgb(h, 1, 0.8)
+#     h += 1/n
+#     t.color(c)
     
-    t.forward(i * 2)
-    t.left(145) 
-    t.width(i/100)
+#     t.forward(i * 2)
+#     t.left(145) 
+#     t.width(i/100)
 
-turtle.done()
+# turtle.done()
+
+def welcome(city="India"):
+    print("Welcome to", city)
+
+welcome("Mumbai") # Output: Welcome to Mumbai
+welcome()         # Output: Welcome to India
