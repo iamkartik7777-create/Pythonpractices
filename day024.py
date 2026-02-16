56e36dd05bff555da64ed5c00ea86303eb7d3eb8
iamkartik7777-create <iamkartik7777@gmail.com>
2026-02-16 23:42:28 +0530

    0-100 step number Output ( for loop )

--- day024.py
+++ day024.py
@@ -1,6 +1,10 @@
-life = 3
-while life > 0:
-    print("Aap khel rahe ho... Life bachi hai:", life)
-    life = life - 1  # Ek life kam ho gayi
+# life = 3
+# while life > 0:
+#     print("Aap khel rahe ho... Life bachi hai:", life)
+#     life = life - 1  # Ek life kam ho gayi
 
-print("Game Over!")
+# print("Game Over!")
+
+
+for i in range(100):
+    print("Step number:", i)
